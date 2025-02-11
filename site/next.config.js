a6909135d39866caf519dbef223c47750a5942cc
DoubtfulTurnip <23531986+DoubtfulTurnip@users.noreply.github.com>
2025-02-12 11:52:09 +1300

    Update next.config.js

--- site/next.config.js
+++ site/next.config.js
@@ -2,15 +2,15 @@
 
 const nextConfig = {
   env: {
-    name: 'Kasm Technologies',
-    description: 'The official store for Kasm supported workspaces.',
-    icon: '/img/logo.svg',
-    listUrl: 'https://registry.kasmweb.com/',
-    contactUrl: 'https://kasmweb.com/support',
+    name: 'DoubtfulTurnip',
+    description: 'The unofficial store for Kasm supported workspaces.',
+    icon: 'https://avatars.githubusercontent.com/u/23531986?v=4',
+    listUrl: 'https://github.com/DoubtfulTurnip/doubtfulturnip-kasm-registry',
+    contactUrl: 'https://github.com/DoubtfulTurnip/doubtfulturnip-kasm-registry',
   },
   reactStrictMode: true,
   swcMinify: true,
-  basePath: '/kasm-registry/1.0',
+  basePath: '/1.0',
   trailingSlash: true,
   images: {
     unoptimized: true,
