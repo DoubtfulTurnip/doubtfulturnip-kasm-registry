b152d41a21bebb8db1583a9acad03b82e011a840
DoubtfulTurnip <23531986+DoubtfulTurnip@users.noreply.github.com>
2025-02-12 22:37:29 +1300

    Update next.config.js

--- site/next.config.js
+++ site/next.config.js
@@ -7,11 +7,10 @@ const nextConfig = {
     icon: '/img/test.jpg',
     listUrl: 'https://github.com/DoubtfulTurnip/doubtfulturnip-kasm-registry',
     contactUrl: 'https://github.com/DoubtfulTurnip/doubtfulturnip-kasm-registry',
-    schema: '1.1',
   },
   reactStrictMode: true,
   swcMinify: true,
-  basePath: '/doubtfulturnip-kasm-registry/1.1',
+  basePath: '/doubtfulturnip-kasm-registry/1.1/',
   trailingSlash: true,
   images: {
     unoptimized: true,
